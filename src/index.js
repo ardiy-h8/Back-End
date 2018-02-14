f751657ac2bc06912bda2678aefcc12662a93553
Yofriadi Yahya <yofriadiyahya@gmail.com>
2018-02-15 06:31:04 +0700

    Fix folder extract

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import bodyParser from 'body-parser'
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
 import cors from 'cors'
 import morgan from 'morgan'
+import fs from 'fs'
+import https from 'https'
 
 import { mongoose, connection } from './config'
 import schema from './schema'
@@ -22,8 +24,14 @@ app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
 connection.on('error', () => console.log('Error connecting to database'))
 connection.on('open', () => console.log('Connected to database'))
 
-if (process.env.NODE_ENV !== 'test') {
-  app.listen(port, () => console.log(`API started on port ${port}`))
-}
-
-export default app
+https
+  .createServer(
+    {
+      key: fs.readFileSync('/etc/letsencrypt/live/ardy.dhnlr.com/privkey.pem'),
+      cert: fs.readFileSync(
+        '/etc/letsencrypt/live/ardy.dhnlr.com/fullchain.pem'
+      )
+    },
+    app
+  )
+  .listen(443)
